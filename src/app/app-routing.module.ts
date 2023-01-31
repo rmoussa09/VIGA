f84f733e2eaa7bafd9a40f485a854d11d4650af0
Farid Baalbaki <baalbakifarid@gmail.com>
2023-01-30 23:50:49 -0500

    created different routes for pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { Game1Component } from './game1/game1.component';
+import { Game2Component } from './game2/game2.component';
+import { Game3Component } from './game3/game3.component';
+
+const routes: Routes = [
+  {path: 'game1', component: Game1Component},
+  {path: 'game2', component: Game2Component},
+  {path: 'game3', component: Game3Component},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [Game1Component, Game2Component, Game3Component]
