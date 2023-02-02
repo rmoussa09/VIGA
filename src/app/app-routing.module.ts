03d7cf6c500fec6ca91694fd2f1227eb4d7372bc
rmoussa09 <rajamoussa09@outlook.com>
2023-02-02 16:07:54 -0500

    Added center logo w/hyperlink, updated components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { Game1Component } from './game1/game1.component';
 import { Game2Component } from './game2/game2.component';
 import { Game3Component } from './game3/game3.component';
-import { AboutusComponent } from './aboutus/aboutus.component';
 import { RegisterComponent } from './register/register.component';
 
 import { LoginComponent } from './login/login.component';
@@ -13,7 +12,6 @@ const routes: Routes = [
   {path: 'game1', component: Game1Component},
   {path: 'game2', component: Game2Component},
   {path: 'game3', component: Game3Component},
-  {path: 'aboutus', component: AboutusComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
 ];
@@ -23,4 +21,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [Game1Component, Game2Component, Game3Component, AboutusComponent, LoginComponent, RegisterComponent]
+export const routingComponents = [Game1Component, Game2Component, Game3Component, LoginComponent, RegisterComponent]
