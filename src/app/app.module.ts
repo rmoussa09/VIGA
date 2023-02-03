afd460de71f42d7208fa8f68bc6e6b7bf3462696
rmoussa09 <rajamoussa09@outlook.com>
2023-02-02 22:00:18 -0500

    Styled Home and Game Pages, added Game Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
