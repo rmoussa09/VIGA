e56d3a0b6ef1f4622e65dd5e220f7decb7575096
Raja Moussa <rajamoussa09@outlook.com>
2023-03-08 13:22:05 -0500

    moving code and creating game comp

--- src/app/routes/game1/game1.component.ts
+++ src/app/routes/game1/game1.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { GuessanimalComponent } from '../../components/games/animalGame/guessanimal/guessanimal.component';
+import { GuessanimalComponent } from '../../components/games/animalGame/animalGame/guessanimal/guessanimal.component';
 
 @Component({
   selector: 'app-game1',
